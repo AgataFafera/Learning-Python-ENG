f5123814d06dbc314ea2604422d34c73b3431672
Agata Fąfera <129389777+AgataFafera@users.noreply.github.com>
2023-07-17 17:35:09 +0200

    Update palindromes.py

--- palindromes.py
+++ palindromes.py
@@ -4,7 +4,6 @@ def remove_whitespace_punctuation(text):
     return text
 
 def palindrome(word):
-    #clean_str = remove_whitespace_punctuation(word).lower()
     clean_str = ''.join( [ char for char in word if char not in string.punctuation+' ' ] )
     reversed_str = clean_str[::-1]
     if reversed_str == clean_str:
